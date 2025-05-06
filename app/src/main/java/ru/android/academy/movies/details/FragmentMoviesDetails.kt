e26a01fef5398f5d2b2f91f22cb55940485dcc42
Legat <Patricul@yandex.ru>
2025-05-06 23:10:35 +0300

    - Commit Home Work 6 (Architecture). test commit

--- app/src/main/java/ru/android/academy/movies/details/FragmentMoviesDetails.kt
+++ app/src/main/java/ru/android/academy/movies/details/FragmentMoviesDetails.kt
@@ -40,7 +40,6 @@ class FragmentMoviesDetails : Fragment() {
         viewModel.movieDetails.observe(this.viewLifecycleOwner, this::setMovie)
 
         viewModel.getMovie(id, requireContext())
-        // tam
     }
 
 
